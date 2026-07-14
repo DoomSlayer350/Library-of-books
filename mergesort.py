99bbdcc89080b50270a972674af91b925046c478
DoomSlayer350 <bashartullah123@gmail.com>
2026-07-14 12:59:18 +0100

    finished the mergesort

--- mergesort.py
+++ mergesort.py
@@ -1,25 +1,20 @@
+import random
+
 def merge(LeftHalf, RightHalf):
 
     LeftIndex = RightIndex = 0
     Result = []
 
-    if len(LeftHalf) <= 1 and len(RightHalf) <= 1: #if they are lists with 1 element
-        if LeftHalf[0] <= RightHalf[0]:
-            Result.extend(LeftHalf)
-            Result.extend(RightHalf)
-            return Result
-        
-        Result.extend(RightHalf)
-        Result.extend(LeftHalf)
-        return Result
-
     while LeftIndex < len(LeftHalf) and RightIndex < len(RightHalf): #We're finding at what point in the 2 lists does the left side <= right side
-        if LeftHalf[LeftIndex] <= RightHalf[RightIndex]:
+        if LeftHalf[LeftIndex] < RightHalf[RightIndex]:
+            Result.append(LeftHalf[LeftIndex])
             LeftIndex += 1
-        elif LeftHalf[LeftIndex] > RightHalf[RightIndex]:
+        else:
+            Result.append(RightHalf[RightIndex])
             RightIndex += 1
+
     Result.extend(LeftHalf[LeftIndex:])
-    Result.extend(RightHalf[:RightIndex]) # At those 2 indexes merge the 2 lists
+    Result.extend(RightHalf[RightIndex:])
     return Result
 
 
@@ -34,17 +29,14 @@ def MergeSort(array):
 
     SortedLeftHalf = MergeSort(LeftHalf)
     SortedRightHalf = MergeSort(RightHalf)
-    print(SortedLeftHalf, SortedRightHalf)
-    print(merge(SortedLeftHalf, SortedRightHalf))
     return merge(SortedLeftHalf, SortedRightHalf)
 
 
-array = [5,6,1,2,4,3,7]
+array = []
 
 """
-testleft = [1,2,3]
-testright = [3,4,5]
-merge(testleft, testright)
+for i in range(1,200,1):
+    array.append(random.randint(1,100))
 """
     
-MergeSort(array)
+print(MergeSort(array))
