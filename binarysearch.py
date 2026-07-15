9051d6b6d0388a1878cb918173dc4a9a19a67a5a
DoomSlayer350 <bashartullah123@gmail.com>
2026-07-15 12:00:30 +0100

    Added a binary search, and renamed the old code (redo.py) to oldCode

--- binarysearch.py
+++ binarysearch.py
@@ -1,8 +1,25 @@
 import random
-import time
+
+def CheckForCutoffIndex(array, target, Midpoint):
+    #check right
+    LeftCutoffPoint = RightCutoffPoint = 0
+    for index in range(Midpoint, len(array) - 1):
+
+        if array[index] != target:
+            RightCutoffPoint = index - 1
+            break
+
+    #check left
+
+    for index in range(Midpoint, 0, -1):
+
+        if array[index] != target:
+            LeftCutoffPoint = index + 1
+            break
+    
+    return RightCutoffPoint, LeftCutoffPoint
 
 def search(array, target):
-    time.sleep(1)
     print(array)
     if len(array) <= 1:
         return array
@@ -12,22 +29,25 @@ def search(array, target):
     SubArray = []
 
     if target > Median:
-        RightHalf = array[Median:] #Get the right half
+        RightHalf = array[Midpoint:] #Get the right half
         SubArray = RightHalf
     elif target < Median:
-        LeftHalf = array[:Median]
+        LeftHalf = array[:Midpoint]
         SubArray = LeftHalf
     else:
-        SubArray = []
+        RightCutoffPoint, LeftCutoffPoint = CheckForCutoffIndex(array, target, Midpoint)
+        SubArray = array[LeftCutoffPoint:(RightCutoffPoint + 1)]
+        return SubArray
         #Implement code to check both left and right and find the indexes until the value != target so we find this range and then return that
 
     return search(SubArray, target)
 
-array = []
-
+#array = []
+"""
 for i in range(1,100):
     array.append(random.randint(1,200))
+"""
+#array = [1,2,5,6,7,8,9,10,11,11,11,11,11,44,44,44,44,44,44,44,44,44,100,102,103,400]
+#target = 44
 
-target = random.randint(1,200)
-
-print(search(array,target))
+#print(search(array,target))
